9c56ef890bea792005aebcb7b338cb5102dae50b
Justin Leung <Justin.Leung@s2partnership.co.uk>
2020-06-16 07:05:58 +0100

    test readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# roadmap" 
+# Roadmap
+
+Test account
+email: test@test.com
+password: T123456
+
+![](public/img/delivery_timeline.png)
--- application/view/home/roadmap.php
+++ application/view/home/roadmap.php
@@ -57,162 +57,161 @@ echo '
     </div>
     <div class="tabbable row justify-content-md-center">';
     
-if ($milestones) {
-    $i = 0;
-    $count = count($milestones);
-    foreach ($milestones as $key => $value) {
-        // calculate the total story point per milestone version
-        $total_time = 0;
-
-        // if (is_array($value['jobs']) && count($value['jobs']) > 0) {
-        //     foreach($value['jobs'] as $t) {
-        //         $total_time += $t['story_points'];
-        //     }
-        // }
-        
-        $panel_css = '';
-        if (isset($value['story_points']) && $value['id'] != 999) {
-            if ($total_time > $value['story_points']) {
-                $panel_css .= 'border: 2px solid #DE4511; background: #F2DEDE';
-            } else {
-                $panel_css .= 'border: 2px solid #A5CB18; background: #FFF';
-            }
-        } else {
-            $panel_css .= 'border: 2px solid ';
-        }
-        
-        $class ='';
-        if ($count == 3) {
-            $class = 'col-md-4';
-        } elseif ($count == 2) {
-            $class = 'col-md-6';
-        } else {
-            $class = 'col-md-12';
-        }
-        $div_name = ($value['id'] == 999 ? 'plan_of_intent' : ($i == 0 ? 'plan_of_commitment' : 'plan_of_record'));
-        echo '
-        <div class="panel-group ' . $class . ' padding-5" id="accordion-' . $value['id'] . '" >
-            <input type="hidden" class="hidden_milestone_name" id="hidden-name-' . $value['id'] . '" value="' . $value['name'] . '" />
-            <div class="panel panel-default" style="' . $panel_css . '">
-                <div class="panel-heading ' . $div_name . '">
-                    <h4 class="pull-right margin-top-5 story_points_holder" id="story_points_' . $value['id'] . '">Total Time Available <span class="total_ticket_time">' . $total_time . '</span>'
-            . ($value['id'] != 999 && $i != 2 ? ' / <span class="total_story_points">' . $value['story_points'] . '</span>' : '' ) . ' </h4>
-
-                    <h3 class="panel-title">';
-                    
-        if ($value['id'] == 999) {
-            echo $value['name'] . '<br><strong>All Future Identified Work, by Milestone</strong>';
-        } else {
-            echo ($i == 0 ? 'Plan of Commitment ' : 'Plan of Record ') . '<br><strong>' . $value['name'] . '</strong>';
-        }
-        echo '</h3>
-                </div>';
+        if ($milestones) {
+            $i = 0;
+            $count = count($milestones);
+            foreach ($milestones as $key => $value) {
+                // calculate the total story point per milestone version
+                $total_time = 0;
+                if (is_array($value['jobs']) && count($value['jobs']) > 0) {
+                    foreach ($value['jobs'] as $t) {
+                        $total_time += $t['estimate'];
+                    }
+                }
                 
-        if ($value['id'] != 999) {
-            echo '
-                <div id="milestone-' . $value['id'] . '">
-                    <div class="panel-body">';
-                    
-            if (isset($value['end_date'])) {
+                $panel_css = '';
+                if (isset($value['story_points']) && $value['id'] != 999) {
+                    if ($total_time > $value['story_points']) {
+                        $panel_css .= 'border: 2px solid #DE4511; background: #F2DEDE';
+                    } else {
+                        $panel_css .= 'border: 2px solid #A5CB18; background: #FFF';
+                    }
+                } else {
+                    $panel_css .= 'border: 2px solid ';
+                }
+                
+                $class ='';
+                if ($count == 3) {
+                    $class = 'col-md-4';
+                } elseif ($count == 2) {
+                    $class = 'col-md-6';
+                } else {
+                    $class = 'col-md-12';
+                }
+                $div_name = ($value['id'] == 999 ? 'plan_of_intent' : ($i == 0 ? 'plan_of_commitment' : 'plan_of_record'));
                 echo '
-                        <div class="milestone_date"><h3>' . date("dS F Y ", strtotime($value['start_date'])) . ' - '. date("dS F Y ", strtotime($value['end_date'])) . '</h3></div>';
-            }
-            echo '
-                        <table id="table-' . $div_name . '" class="roadmap_table table">
-                            <thead>
-                                <tr>
-                                    <th class="name">Epic</th>
-                                    <th class="confidence_level">Confidence Level</th>
-                                    <th class="story_points">Estimate</th>
-                                </tr>
-                            </thead>';
+                <div class="panel-group ' . $class . ' padding-5" id="accordion-' . $value['id'] . '" >
+                    <input type="hidden" class="hidden_milestone_name" id="hidden-name-' . $value['id'] . '" value="' . $value['name'] . '" />
+                    <div class="panel panel-default" style="' . $panel_css . '">
+                        <div class="panel-heading ' . $div_name . '">
+                            <h4 class="pull-right margin-top-5 story_points_holder" id="story_points_' . $value['id'] . '">Total Time Available <span class="total_ticket_time">' . $total_time . '</span>'
+                    . ($value['id'] != 999 && $i != 2 ? ' / <span class="total_story_points">' . $value['story_points'] . '</span>' : '' ) . ' </h4>
+
+                            <h3 class="panel-title">';
                             
-            //check jobs
-            if (is_array($value['jobs']) && count($value['jobs']) > 0) {
-                echo '
-                            <tbody id="drag_drop_parent_' . $value['id'] . '" ' . ($count > 1 ? (!is_array($value['jobs']) ? 'class="connectedSortable sortable_drag"' : 'class="connectedSortable"') : '') . '>';
-                                    
-                foreach($value['jobs'] as $job) {
-                    echo '
-                                <tr id="drag_drop_item_' . $job['id'] . '" data-milestone="' . $value['id'] . '" style="display: table-row;">
-                                    <td class="name padding-top-5"><a href="/job/view/'. $job['id'] .'" target="_blank"><span class="glyphicon btn btn-xs ' . $priorities[$job['priority']] . '" aria-hidden="true"></span></a> ' . $job['name'] . '</td>
-                                    <td class="confidence_level padding-top-5">' . $job['confidence_level'] . '</td>
-                                    <td class="story_points padding-top-5">' . $job['estimate'] . '</td>
-                
-                                </tr>';
+                if ($value['id'] == 999) {
+                    echo $value['name'] . '<br><strong>All Future Identified Work, by Milestone</strong>';
+                } else {
+                    echo ($i == 0 ? 'Plan of Commitment ' : 'Plan of Record ') . '<br><strong>' . $value['name'] . '</strong>';
                 }
-                echo '
-                            </tbody>';
-            } else {
-                echo '
-                            <tbody id="drag_drop_parent_' . $value['id'] . '" class="connectedSortable ui-sortable">
-                                <tr class="empty_tr">
-                                    <td class="name"></td>
-                                    <td class="story_points"></td>
-                                    <td class="status"></td>
-                                </tr>
-                            </tbody>';
-            }
-            echo '
-                        </table>
-                    </div>
-                </div>';
-        } else {
-            if (isset($future_milestones[0]['id'])) {
-                $form->futureMilestoneTable($future_milestones, $priorities);
-            }
-            echo '
-                <div id="milestone-' . $value['id'] . '">
-                    <div class="panel-body">
-                    <div class="milestone_date"><h3>All Other Epics</h3></div>
-                        <table id="table-' . $div_name . '" class="roadmap_table table">
-                            <thead>
-                                <tr>
-                                    <th class="name">Epic</th>
-                                    <th class="confidence_level">Confidence Level</th>
-                                    <th class="story_points">Estimate</th>
-                                    <th class="status">Status</th>
-                                </tr>
-                            </thead>';
+                echo '</h3>
+                        </div>';
                         
-            //check jobs
-            if (is_array($value['jobs']) && count($value['jobs']) > 0) {
-                echo '
-                            <tbody id="drag_drop_parent_' . $value['id'] . '" ' . ($count > 1 ? (!is_array($value['jobs']) ? 'class="connectedSortable sortable_drag"' : 'class="connectedSortable"') : '') . '>';
-                                    
-                foreach($value['jobs'] as $job) {
+                if ($value['id'] != 999) {
                     echo '
-                                <tr id="drag_drop_item_' . $job['id'] . '" data-milestone="' . $value['id'] . '" style="display: table-row;">
-                                    <td class="name padding-top-5"><a href="/job/view/'. $job['id'] .'" target="_blank"><span class="glyphicon btn btn-xs ' . $priorities[$job['priority']] . '" aria-hidden="true"></span></a> ' . $job['name'] . '</td>
-                                    <td class="confidence_level padding-top-5">' . $job['confidence_level'] . '</td>
-                                    <td class="story_points padding-top-5">' . $job['story_points'] . '</td>
-                                    <td class="status padding-top-5">' . $job['status'] . '</td>
-                                </tr>';
+                        <div id="milestone-' . $value['id'] . '">
+                            <div class="panel-body">';
+                            
+                    if (isset($value['end_date'])) {
+                        echo '
+                                <div class="milestone_date"><h3>' . date("dS F Y ", strtotime($value['start_date'])) . ' - '. date("dS F Y ", strtotime($value['end_date'])) . '</h3></div>';
+                    }
+                    echo '
+                                <table id="table-' . $div_name . '" class="roadmap_table table">
+                                    <thead>
+                                        <tr>
+                                            <th class="name">Epic</th>
+                                            <th class="confidence_level">Confidence Level</th>
+                                            <th class="story_points">Estimate</th>
+                                        </tr>
+                                    </thead>';
+                                    
+                    //check jobs
+                    if (is_array($value['jobs']) && count($value['jobs']) > 0) {
+                        echo '
+                                    <tbody id="drag_drop_parent_' . $value['id'] . '" ' . ($count > 1 ? (!is_array($value['jobs']) ? 'class="connectedSortable sortable_drag"' : 'class="connectedSortable"') : '') . '>';
+                                            
+                        foreach ($value['jobs'] as $job) {
+                            echo '
+                                        <tr id="drag_drop_item_' . $job['id'] . '" data-milestone="' . $value['id'] . '" style="display: table-row;">
+                                            <td class="name padding-top-5"><a href="/job/view/'. $job['id'] .'" target="_blank"><span class="glyphicon btn btn-xs ' . $priorities[$job['priority']] . '" aria-hidden="true"></span></a> ' . $job['name'] . '</td>
+                                            <td class="confidence_level padding-top-5">' . $job['confidence_level'] . '</td>
+                                            <td class="story_points padding-top-5">' . $job['estimate'] . '</td>
+                        
+                                        </tr>';
+                        }
+                        echo '
+                                    </tbody>';
+                    } else {
+                        echo '
+                                    <tbody id="drag_drop_parent_' . $value['id'] . '" class="connectedSortable ui-sortable">
+                                        <tr class="empty_tr">
+                                            <td class="name"></td>
+                                            <td class="story_points"></td>
+                                            <td class="status"></td>
+                                        </tr>
+                                    </tbody>';
+                    }
+                    echo '
+                                </table>
+                            </div>
+                        </div>';
+                } else {
+                    if (isset($future_milestones[0]['id'])) {
+                        $form->futureMilestoneTable($future_milestones, $priorities);
+                    }
+                    echo '
+                        <div id="milestone-' . $value['id'] . '">
+                            <div class="panel-body">
+                            <div class="milestone_date"><h3>All Other Epics</h3></div>
+                                <table id="table-' . $div_name . '" class="roadmap_table table">
+                                    <thead>
+                                        <tr>
+                                            <th class="name">Epic</th>
+                                            <th class="confidence_level">Confidence Level</th>
+                                            <th class="story_points">Estimate</th>
+                                            <th class="status">Status</th>
+                                        </tr>
+                                    </thead>';
+                                
+                    //check jobs
+                    if (is_array($value['jobs']) && count($value['jobs']) > 0) {
+                        echo '
+                                    <tbody id="drag_drop_parent_' . $value['id'] . '" ' . ($count > 1 ? (!is_array($value['jobs']) ? 'class="connectedSortable sortable_drag"' : 'class="connectedSortable"') : '') . '>';
+                                            
+                        foreach ($value['jobs'] as $job) {
+                            echo '
+                                        <tr id="drag_drop_item_' . $job['id'] . '" data-milestone="' . $value['id'] . '" style="display: table-row;">
+                                            <td class="name padding-top-5"><a href="/job/view/'. $job['id'] .'" target="_blank"><span class="glyphicon btn btn-xs ' . $priorities[$job['priority']] . '" aria-hidden="true"></span></a> ' . $job['name'] . '</td>
+                                            <td class="confidence_level padding-top-5">' . $job['confidence_level'] . '</td>
+                                            <td class="story_points padding-top-5">' . $job['story_points'] . '</td>
+                                            <td class="status padding-top-5">' . $job['status'] . '</td>
+                                        </tr>';
+                        }
+                        echo '
+                                    </tbody>';
+                    } else {
+                        echo '
+                                    <tbody id="drag_drop_parent_' . $value['id'] . '" class="connectedSortable ui-sortable">
+                                        <tr class="empty_tr">
+                                            <td class="name"></td>
+                                            <td class="story_points"></td>
+                                            <td class="status"></td>
+                                        </tr>
+                                    </tbody>';
+                    }
+                    echo '
+                                </table>
+                            </div>
+                        </div>';
                 }
                 echo '
-                            </tbody>';
-            } else {
-                echo '
-                            <tbody id="drag_drop_parent_' . $value['id'] . '" class="connectedSortable ui-sortable">
-                                <tr class="empty_tr">
-                                    <td class="name"></td>
-                                    <td class="story_points"></td>
-                                    <td class="status"></td>
-                                </tr>
-                            </tbody>';
-            }
-            echo '
-                        </table>
                     </div>
                 </div>';
+                
+                $i++;
+            }
         }
-        echo '
-            </div>
-        </div>';
-        
-        $i++;
-    }
-}
 echo '
     </div>
 </div>';
